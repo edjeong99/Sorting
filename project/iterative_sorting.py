95aaad4fae723973c1663ba0e6824d20d0db716a
Edward J <edjeong99@gmail.com>
2019-02-19 15:53:11 -0500

    finish bubble sort

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -36,7 +36,10 @@ def insertion_sort(arr):
 
 # STRETCH: implement the Bubble Sort function below
 def bubble_sort(arr):
-
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 
