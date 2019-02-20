76701862891fe9b2d55d3bfb71489386b1bb650b
Edward J <edjeong99@gmail.com>
2019-02-19 19:06:18 -0500

    merge sort in place

--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -33,13 +33,31 @@ def merge_sort(arr):
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
+    # since arrA and arrB already sorted, we only need to compare the first element of each when merging!
+    for i in range(0, end - start):
+        if start >= mid:    # all elements in arrA have been merged
+            arr[i], arr[mid+1] = arr[mid+1], arr[i]
+            mid += 1
+        elif mid+1 >= end:  # all elements in arrB have been merged
+            arr[i], arr[start] = arr[start], arr[i]
+            start += 1
+        elif arr[start] < arr[mid+1]:  # next element in arrA smaller, so add to final array
+            arr[i] = arr[start]
+            start += 1
+        else:  # else, next element in arrB must be smaller, so add it to final array
+            arr[i] = arr[mid + 1]
+            mid += 1
 
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
     # TO-DO
-
+    if l < r:
+        middle = (l + r) // 2
+        merge_sort_in_place(arr, l, middle)
+        merge_sort_in_place(arr, middle + 1, r)
+        merge_in_place(arr, l, middle, r)
     return arr
 
 
@@ -68,6 +86,7 @@ def partition(arr, low, high):
         if arr[j] <= pivot:
             arr[i], arr[j] = arr[j], arr[i]
             i += 1
+
     arr[i], arr[high] = arr[high], arr[i]
     return (i)
 
